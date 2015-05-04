5394aaf9f33c0d56ce2ffb2f19fc6047edba85b0
Domenic Murtari <domenic.murtari@gmail.com>
2015-05-04 16:16:59 -0600

    Basic view added

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var app = express()
+var _ = require('lodash')
+
+// use jade as the view engine
+app.set('view engine', 'jade')
+
+// set where the static contents are (e.g., css, js)
+app.use(express.static(__dirname + '/public'))
+
+// default to index
+app.get('/', function(req, res) {
+    res.render('index.jade')
+})
+
+// load routes
+
+
+app.set('port', (process.env.PORT || 3000))
+
+var server = app.listen(app.get('port'), function() {
+
+    var host = server.address().address
+    var port = server.address().port
+    console.log('App listening at http://%s:%s', host, port)
+})
